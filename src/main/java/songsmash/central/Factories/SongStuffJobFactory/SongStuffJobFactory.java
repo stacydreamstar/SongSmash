d4a7ee2b76cbeefd42ad58179afacf745cd05685
stacydreamstar <stacydreamstar@gmail.com>
2025-09-13 00:28:44 -0500

    Stillcookin

--- src/main/java/songsmash/central/Factories/SongStuffJobFactory/SongStuffJobFactory.java
+++ src/main/java/songsmash/central/Factories/SongStuffJobFactory/SongStuffJobFactory.java
@@ -38,17 +38,68 @@ import java.util.UUID;
 @EnableBatchProcessing
 public class SongStuffJobFactory{
 
+    @Autowired
+    JobLauncher jobLauncher;
     String userOption;
-    private JobLauncher jobLauncher;
+    public void setUserOption(String option){
+        this.userOption = option;
+    }
+
+    public List<String> determinerTree(String input, Long trackingId, MultipartFile file){
+
+        switch(input){
+            case "MULTI_PLAYLIST" -> {
+                System.out.println("starting job: " + input);
+
+                try {
+                    JobParameters jobParameters = new JobParametersBuilder()
+                            .addString("fileName", file.getOriginalFilename())
+                            .addString("selectedOption", input)
+                            .addLong("trackingId", trackingId)
+                            .toJobParameters();
+                    JobExecution jobExecution = jobLauncher.run(exampleJob(), jobParameters);
+                    System.out.println("Job Status: " + jobExecution.getStatus());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            case "SUMN_ELSE" -> {
+                System.out.println("output is a List. Elements: " + input);
+                return null;
+            }
+            case "SUMN_ELSEX" -> {
+                System.out.println("output is a File: " + (input));
+                return null;
+            }
+            default -> System.out.println("Input is of type: " + input.getClass().getName());
+            return null;
+
+        }
+
+    }
 
 
 
 
-
+    @Bean("textFileUploadedJob")
     public List<String> textFileUploadedJob(MultipartFile file, String selectedOption) {
-
-
         Long trackingId= Math.abs(UUID.randomUUID().getMostSignificantBits());
+        determinerTree(selectedOption, trackingId, file);
+
+
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("fileName", file.getOriginalFilename())
+                    .addString("selectedOption", selectedOption)
+                    .addLong("trackingId", trackingId)
+                    .toJobParameters();
+
+            JobExecution jobExecution = jobLauncher.run(exampleJob(), jobParameters);
+            System.out.println("Job Status: " + jobExecution.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
